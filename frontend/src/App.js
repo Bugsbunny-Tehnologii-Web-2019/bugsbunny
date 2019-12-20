687660856b5eac94fff4f32b599023cf0f79d740
BiancaHerghelegiu <bianca.herghelegiu98@gmail.com>
2019-12-20 10:29:32 +0000

    Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import AddUser from './AddUser'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <AddUser/>
+      </div>
+    );
+  }
+}
+
+export default App;
